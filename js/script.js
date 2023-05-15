6cfa1a9afaf27281c98cd7a72fe07419504ba3c7
shani-gal-or <shangalor@gmail.com>
2023-05-16 01:31:56 +0300

    last commit

--- js/script.js
+++ js/script.js
@@ -19,3 +19,29 @@ function handaleHamburger(size) {
         $('#hamburgerNavigation').attr('class','w30 hv50 collapse show');
     }
 }
+
+const form = document.getElementById("bookForm");
+const saveButton = document.getElementById("save");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const publicationDateInput = document.getElementById("publicationDate");
+    const selects = document.getElementsByClassName("form-select");
+    const errorContainer = document.getElementById("publicationDateError");
+    const publicationDateValue = publicationDateInput.value;
+    let selectFlag = false;
+    const pattern = /^[\u0590-\u05FF]+ \d{4}$/u;
+    for( let i = 0; i < selects.length; i++){
+        if(selects[i].value === "-1"){
+            console.log(selects[i].value);
+            errorContainer.innerHTML = errorContainer.innerHTML + `<span>${selects[i].name} is required</span>`;
+            selectFlag = true;
+        }
+    }
+    if (!pattern.test(publicationDateValue)) {
+        const errorMessage = "<span>תאריך הוצאה חייב להיות בפורמט: חודש עברי ושנה (לדוגמה: אפריל 2016)</span>";
+        errorContainer.innerHTML = errorContainer.innerHTML + errorMessage;
+        return;
+    }
+    if(selectFlag) return;
+    form.submit();
+});
