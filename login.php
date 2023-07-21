3a11b9c986d05ea353e8c5107ef2da71e57901a2
Ron Barcochva <shoko6662@gmail.com>
2023-07-21 05:36:45 +0300

    vfbf

--- login.php
+++ login.php
@@ -1,8 +1,5 @@
 <?php 
 include "db.php";
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 session_start();//on logout session_destroy();
 if(!empty($_POST["email"])) {
   $email = $_POST["email"];
