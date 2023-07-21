383d2581b6e21e1a64f1f09a7b4b2087ea38e1c0
Ron Barcochva <shoko6662@gmail.com>
2023-07-21 05:44:05 +0300

    fdgd

--- db.php
+++ db.php
@@ -2,6 +2,10 @@
 include "config.php";
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 $connection->query("SET NAMES 'utf8'");
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 //testing connection success
 if(mysqli_connect_errno()) {
     die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")"
